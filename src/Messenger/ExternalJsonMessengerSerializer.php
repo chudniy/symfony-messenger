9244c52c21c3e636529990f373489ab5077eeb70
Yevhen Chudnyi <yevhen.chudnyi@ekreative.com>
2019-11-13 14:18:53 +0200

    Chapter 46-48

--- src/Messenger/ExternalJsonMessengerSerializer.php
+++ src/Messenger/ExternalJsonMessengerSerializer.php
@@ -3,8 +3,10 @@
 namespace App\Messenger;
 
 use App\Message\Command\LogEmoji;
+use Exception;
 use Symfony\Component\Messenger\Envelope;
 use Symfony\Component\Messenger\Exception\MessageDecodingFailedException;
+use Symfony\Component\Messenger\Stamp\BusNameStamp;
 use Symfony\Component\Messenger\Transport\Serialization\SerializerInterface;
 
 class ExternalJsonMessengerSerializer implements SerializerInterface
@@ -27,13 +29,31 @@ class ExternalJsonMessengerSerializer implements SerializerInterface
         $body = $encodedEnvelope['body'];
         $headers = $encodedEnvelope['headers'];
         $data = json_decode($body, true);
-        $message = new LogEmoji($data['emoji']);
 
+        if (null === $data) {
+            throw new MessageDecodingFailedException('Invalid JSON');
+        }
+
+        if (!isset($headers['type'])) {
+            throw new MessageDecodingFailedException('Missing "type" header');
+        }
+        switch ($headers['type']) {
+            case 'emoji':
+                $envelope = $this->createLogEmojiEnvelope($data);
+                break;
+            default:
+                throw new MessageDecodingFailedException(sprintf('Invalid type "%s"', $headers['type']));
+        }
+
+        // in case of redelivery, unserialize any stamps
         $stamps = [];
         if (isset($headers['stamps'])) {
             $stamps = unserialize($headers['stamps']);
         }
-        return new Envelope($message, $stamps);
+
+        $envelope = $envelope->with(... $stamps);
+
+        return $envelope;
     }
 
     /**
@@ -56,8 +76,9 @@ class ExternalJsonMessengerSerializer implements SerializerInterface
         if ($message instanceof LogEmoji) {
             // recreate what the data originally looked like
             $data = ['emoji' => $message->getEmojiIndex()];
+            $type = 'emoji';
         } else {
-            throw new \Exception('Unsupported message class');
+            throw new Exception('Unsupported message class');
         }
         $allStamps = [];
         foreach ($envelope->all() as $stamps) {
@@ -67,8 +88,30 @@ class ExternalJsonMessengerSerializer implements SerializerInterface
             'body' => json_encode($data),
             'headers' => [
                 // store stamps as a header - to be read in decode()
-                'stamps' => serialize($allStamps)
+                'stamps' => serialize($allStamps),
+                'type' => $type,
             ],
         ];
+    }
+
+    /**
+     * @param $data
+     * @param array $stamps
+     * @return Envelope
+     */
+    private function createLogEmojiEnvelope($data): Envelope
+    {
+        if (!isset($data['emoji'])) {
+            throw new MessageDecodingFailedException('Missing the emoji key!');
+        }
+
+        $message = new LogEmoji($data['emoji']);
+
+        $envelope = new Envelope($message);
+
+        // needed only if you need this to be sent through the non-default bus
+        $envelope = $envelope->with(new BusNameStamp('command.bus'));
+
+        return $envelope;
     }
 }
